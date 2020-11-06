7a42dd5d30dc172bc45ec28259f2f8f26f68b3ec
Segawa Shoko <shoko4prog@gmail.com>
2020-11-07 07:25:56 +0900

    初期状態

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,12 @@
+@section('header')
+    <div id="header">
+        <p id="logo">logo</p>
+        <nav>
+            <ul>
+                <a href="#"><li>ホーム</li></a>
+                <a href="#"><li>質問する</li></a>
+                <a href="#"><li>マイページ</li></a>
+            </ul>
+        </nav>
+    </div>
+@endsection
